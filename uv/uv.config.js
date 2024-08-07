37d27ce2884e7a98bea09617fd727c9f4a46f833
opal1056 <vishwa.cool05@gmail.com>
2024-08-07 19:11:18 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/service/',
-    bare: '/bare/',
+    prefix: '/uv/service/',
+    bare: 'https://ruby.rubynetwork.co/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
